1398fdacc2595b0bacbc4ccd64410a2bac0a40cf
A David <aimardavidninor@hotmail.com>
2022-10-10 11:27:35 -0500

    Punto_Cuatro

--- js/datos.js
+++ js/datos.js
@@ -8,39 +8,39 @@ let coloAzu = 0;
 function ColorPla(e) {
     e.preventDefault();
     const numpla = document.getElementById("N_placa");
-    const rta = document.getElementById("rsta")
+    const rspt1 = document.getElementById("rspt1")
     if (parseInt(numpla.value) == 1 || parseInt(numpla.value) == 2) {
         color = "Amarillo";
         coloAma++;
-        rta.textContent = `El color es ${color} y hay ${coloAma}`
+        rspt1.textContent = `El color es ${color} y hay ${coloAma}`
     } else if (parseInt(numpla.value) == 3 || parseInt(numpla.value) == 4) {
         color = "Rosa";
         coloRoS++;
-        rta.textContent = `El color es ${color} y hay ${coloRoS}`
+        rspt1.textContent = `El color es ${color} y hay ${coloRoS}`
     } else if (parseInt(numpla.value) == 5 || parseInt(numpla.value) == 6) {
         color = "Rojo";
         coloRoJ++;
-        rta.textContent = `El color es ${color} y hay ${coloRoJ}`
+        rspt1.textContent = `El color es ${color} y hay ${coloRoJ}`
     } else if (parseInt(numpla.value) == 7 || parseInt(numpla.value) == 8) {
         color = "Verde";
         coloVer++;
-        rta.textContent = `El color es ${color} y hay ${coloVer}`
+        rspt1.textContent = `El color es ${color} y hay ${coloVer}`
     } else if (parseInt(numpla.value) == 9 || parseInt(numpla.value) == 0) {
         color = "Azul";
         coloAzu++;
-        rta.textContent = `El color es ${color} y hay ${coloAzu}`
+        rspt1.textContent = `El color es ${color} y hay ${coloAzu}`
     }
 }
 
 function reincio(e) {
     e.preventDefault();
-    const rta = document.getElementById("rsta")
+    const rspt1 = document.getElementById("rspt1")
     coloAma = 0;
     coloRoS = 0;
     coloRoJ = 0;
     coloVer = 0;
     coloAzu = 0;
-    rta.textContent = "";
+    rspt1.textContent = "";
 }
 
 
@@ -52,19 +52,19 @@ let a3 = 0;
 function CalcPEdad(e){
     e.preventDefault();
     const ed = document.getElementById("E_Animal");
-    const rsp = document.getElementById("rst")
+    const rspt2 = document.getElementById("rspt2")
     if (parseInt(ed.value) <= 1){
         a1++;
         ta++;
-        rsp.textContent = "Guardado con exito en Jovenes"
+        rspt2.textContent = "Guardado con exito en Jovenes"
     }else if (parseInt(ed.value) > 1 && parseInt(ed.value) <= 3){
         a2++;
         ta++;
-        rsp.textContent = "Guardado con exito en Adultos"
+        rspt2.textContent = "Guardado con exito en Adultos"
     } else if (parseInt(ed.value) > 3){
         a3++;
         ta++;
-        rsp.textContent = "Guardado con exito en ancianos"
+        rspt2.textContent = "Guardado con exito en ancianos"
     }else{
         rsp.textContent = "porfavor ingrese una edad"
     }
@@ -74,11 +74,11 @@ let p2 = 0;
 let p3 = 0;
 function Porcentages(e) {
     e.preventDefault();
-    const rsp = document.getElementById("rst")
+    const rspt2 = document.getElementById("rspt2")
     p1 = (a1/ta)*100;
     p2 = (a2/ta)*100;
     p3 = (a3/ta)*100;
-    rsp.textContent = `Los porcentajes de animales por edad son:
+    rspt2.textContent = `Los porcentajes de animales por edad son:
     ${p1.toFixed(2)}% para los jovenes,
     ${p2.toFixed(2)}% para los adultos,
     ${p3.toFixed(2)}% para los ancianos.`;
@@ -86,30 +86,86 @@ function Porcentages(e) {
 
 function reincio2(e) {
     e.preventDefault();
-    const rta = document.getElementById("rst")
-    let p1 = 0;
-    let p2 = 0;
-    let p3 = 0;
-    rsp.textContent = "";
+    const rspt2 = document.getElementById("rspt2")
+    p1 = 0;
+    p2 = 0;
+    p3 = 0;
+    rspt2.textContent = "";
 }
 
 //Ejercicio 3
 let sa = 0;
 function salario(e) {
     e.preventDefault();
-    const rs = document.getElementById("rsp")
+    const rspt3 = document.getElementById("rspt3")
     const ht = document.getElementById("H_traba")
     if (parseInt(ht.value) <= 40) {
         sa = ht.value * 20;
-        rs.textContent = `El salario de esta semana es ${sa}`;
+        rspt3.textContent = `El salario de esta semana es ${sa}`;
     }else{
         sa = 800 + ( (ht.value - 40) * 25);
-        rs.textContent = `El salario de esta semana es ${sa}`;
+        rspt3.textContent = `El salario de esta semana es ${sa}`;
     }
 }
 function reincio3(e) {
     e.preventDefault();
-    const rs = document.getElementById("rsp")
-    let sa = 0;
-    rsp.textContent = "";
+    const rspt3 = document.getElementById("rspt3")
+    sa = 0;
+    rspt3.textContent = "";
 }
+
+
+//Ejercicio 4
+let edm = 0;
+let edf = 0;
+let edt = 0;
+let nh = 0;
+let nm = 0;
+let nt = 0;
+let eph = 0;
+let epm = 0;
+let epg = 0;
+function Guard_edades(e) {
+    e.preventDefault();
+    const rspt4 = document.getElementById("rspt4")
+    const gender = document.getElementById("Gender")
+    const edad = document.getElementById("Edad")
+    if ( gender.value == "Male" ){
+        edm = parseInt(edad.value);
+        edt = parseInt(edad.value);
+        nh++;
+        nt++;
+        rspt4.textContent = "Guardado con exito M"
+    }else if ( gender.value == "Female"){
+        edf = parseInt(edad.value);
+        edt = parseInt(edad.value);
+        nm++;
+        nt++;
+        rspt4.textContent = "Guardado con exito F"
+    }else {
+        rspt4.textContent = "Por favor Elija el genero"
+    }
+}
+
+function Promedio(e) {
+    e.preventDefault();
+    const rspt4 = document.getElementById("rspt4")
+    eph = edm / nh ;
+    epm = edf / nm ;
+    epg = edt / nt ;
+    rspt4.textContent = `Las Edades Promedio son: 
+    ${eph} Para los hombres 
+    ${epm} Para Las mujeres
+    ${epg} Para el grupo`
+}
+
+function reincio4(e) {
+    e.preventDefault();
+    const rspt4 = document.getElementById("rspt4")
+    edm = 0;
+    edf = 0;
+    edp = 0;
+    eph = 0;
+    epm = 0;
+    rspt4.textContent = ""
+} 
